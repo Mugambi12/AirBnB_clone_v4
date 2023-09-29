5c163bec79b272f7fb2f3a4bc91dd83b0661ef07
mugambi12 <silasmungiria16@gmail.com>
2023-09-29 13:28:53 +0300

    Improve storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -81,8 +81,31 @@ class TestFileStorage(unittest.TestCase):
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_new(self):
-        """test that new adds an object to the database"""
+        """Test that new adds an object to the database"""
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    def test_get(self):
+        """Test method for obtaining an instance db storange"""
+        storage = DBStorage()
+        dic = {"name": "Cundinamarca"}
+        instance = State(**disc)
+        storage.new(instance)
+        storage.save()
+        get_instance = storage.get(State, instance.id)
+        self.assertEqual(get_instance, instance)
+
+    def test_count(self):
+        """Tests count method db storage """
+        storage = DBStorage()
+        dic = {"name": "Vecindad"}
+        state = State(**disc)
+        storage.new(state)
+        dic = {"name": "Mexico", "state_id": state.id}
+        city = City(**dic)
+        storage.new(city)
+        statoge.save()
+        c = storage.count()
+        self.assertEqual(len(storage.all()), c)
