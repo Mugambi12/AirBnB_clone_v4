ee7e05c9ea55e6eb05c6072893e3429ae92b5d18
mugambi12 <silasmungiria16@gmail.com>
2023-10-03 12:53:00 +0300

    updates

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
+import flask
 from flask import Blueprint
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-"""import storage engine and classes"""
 from models import storage
 from models.state import State
 from models.city import City
@@ -10,7 +9,6 @@ from models.user import User
 from models.place import Place
 from models.review import Review
 
-"""import flask views"""
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -18,3 +16,5 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
