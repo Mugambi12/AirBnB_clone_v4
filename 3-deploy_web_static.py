29f0971c5b93ed2aacd24f5632a89d9a221fbf4c
mugambi12 <silasmungiria16@gmail.com>
2023-10-09 11:59:06 +0300

    updates

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['54.197.105.254', '100.24.206.145']
 
 
 def do_pack():
@@ -19,15 +19,16 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except:
+    except Exception as e:
+        print(f"Error in do_pack: {e}")
         return None
 
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        return False
     try:
+        if exists(archive_path) is False:
+            return False
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
@@ -40,7 +41,8 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except Exception as e:
+        print(f"Error in do_deploy: {e}")
         return False
 
 
